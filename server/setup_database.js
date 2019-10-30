f03a57a0c65a7eafb5fd3db1e08ca51fde67bfc0
Kenton Carrier <kncarrier28@gmail.com>
2019-10-30 17:38:39 -0400

    fix: Fix syntax error with sql script

--- server/setup_database.js
+++ server/setup_database.js
@@ -5,12 +5,6 @@ var con = mysql.createConnection({
   user: "geoffrey",
   password: "Ya9pQcM3x7BsRqDX"
 });
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-
 const createDBString =
   "DROP DATABASE IF EXISTS toyzrus; CREATE DATABASE toyzrus; USE toyzrus;";
 const createUserTableString =
@@ -20,21 +14,25 @@ const createItemsTableString =
 const createOrdersTableString =
   "CREATE TABLE orders (orderId INT PRIMARY KEY NOT NULL,uId INT,itemId INT,status VARCHAR(20),quantity INT);";
 
-con.query(createDBString, function(error, results) {
-  if (error) throw error;
-  else console.log("Results: " + results);
-});
-con.query(createUserTableString, function(error, results) {
-  if (error) throw error;
-  else console.log("Results: " + results);
-});
-con.query(createItemsTableString, function(error, results) {
-  if (error) throw error;
-  else console.log("Results: " + results);
-});
-con.query(createOrdersTableString, function(error, results) {
-  if (error) throw error;
-  else console.log("Results: " + results);
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query(createDBString, function(error, results) {
+    if (error) throw error;
+    else console.log("Results: " + results);
+  });
+  con.query(createUserTableString, function(error, results) {
+    if (error) throw error;
+    else console.log("Results: " + results);
+  });
+  con.query(createItemsTableString, function(error, results) {
+    if (error) throw error;
+    else console.log("Results: " + results);
+  });
+  con.query(createOrdersTableString, function(error, results) {
+    if (error) throw error;
+    else console.log("Results: " + results);
+  });
 });
 
 con.end();
