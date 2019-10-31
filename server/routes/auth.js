9d3cdbd99371c782c1dc3b17b3c27517269ee6c2
Kenton Carrier <kncarrier28@gmail.com>
2019-10-30 22:56:25 -0400

    refactor(auth): Add password to sql connection

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,7 +5,7 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "geoffrey",
-  password: "",
+  password: "Ya9pQcM3x7BsRqDX",
   database: "toyzrus"
 });
 
