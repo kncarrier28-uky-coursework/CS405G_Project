3e581a227ec8169fffda68174135a3a285704f4b
Kenton Carrier <kncarrier28@gmail.com>
2019-10-30 22:09:48 -0400

    chore(cors): Implement cors in express app

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 var createError = require("http-errors");
 var express = require("express");
+var cors = require("cors");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
@@ -9,6 +10,10 @@ var usersRouter = require("./routes/users");
 var authRouter = require("./routes/auth");
 
 var app = express();
+
+// cors policy middleware
+app.use(cors());
+app.options("*", cors());
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
