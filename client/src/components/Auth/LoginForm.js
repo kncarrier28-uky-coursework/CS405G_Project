ed25475b995ec24e6b1ae950795d65341d58aa3b
Kenton Carrier <kncarrier28@gmail.com>
2019-10-30 22:38:53 -0400

    feat: Link fetch client from login form to server

--- client/src/components/Auth/LoginForm.js
+++ client/src/components/Auth/LoginForm.js
@@ -1,10 +1,52 @@
 import React from "react";
-import { Field, Control, Label, Input } from "react-bulma-components";
 
 export class LoginForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { username: "", password: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit() {
+    var hashedPass = "";
+    // bcrypt.genSalt(10, (err, salt) => {
+    //   bcrypt.hash(this.state.password, salt, (err, hash) => {
+    //     hashedPass = hash;
+    //   });
+    // });
+    fetch("http://localhost:3010/auth/login", {
+      method: "POST",
+      credentials: "same-origin",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify({
+        userName: this.state.username,
+        password: hashedPass
+      })
+    })
+      .then(res => {
+        console.log(res);
+        return res.json();
+      })
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => console.log(error));
+  }
+
   render() {
     return (
       <div>
@@ -13,18 +55,32 @@ export class LoginForm extends React.Component {
         <div className="field">
           <div className="control">
             <label className="label">Username</label>
-            <input type="text" className="input" placeholder="Username" />
+            <input
+              onChange={this.handleChange}
+              name="username"
+              type="text"
+              value={this.state.username}
+              className="input"
+              placeholder="Username"
+            />
           </div>
         </div>
         <div className="field">
           <div className="control">
             <label className="label">Password</label>
-            <input type="password" className="input" placeholder="Password" />
+            <input
+              name="password"
+              type="password"
+              className="input"
+              value={this.state.password}
+              placeholder="Password"
+              onChange={this.handleChange}
+            />
           </div>
         </div>
         <div className="field">
           <div className="control">
-            <button className="button is-primary" onClick={this.props.onSubmit}>
+            <button className="button is-primary" onClick={this.handleSubmit}>
               Login
             </button>
           </div>
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,43 +1,77 @@
-var express = require("express");
+const express = require("express");
+const bcrypt = require("bcrypt");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "geoffrey",
+  password: "",
+  database: "toyzrus"
+});
+
+const saltRounds = 10;
+
 var router = express.Router();
 
 // req: the username and password of a user
 // res: user object for matching user, session token, and cookie
 // next: if error, error handler route
-router.post("/login", function(req, res, next) {
+router.post("/login", (req, res) => {
   connection.connect();
-  connection.query('SELECT uName, password FROM users WHERE uName=req.body.userName AND password=req.body.password', function (error, results) {
+  const userName = req.body.userName;
+  const password = req.body.password;
+
+  console.log(userName);
+  const queryString = `SELECT * FROM users WHERE uName=${userName}`;
+  connection.query(queryString, function(error, results) {
     if (error) throw error;
-    if(results.length == 0) res.body.error = 'Invalid username and password.';
     else {
-      connection.query('SELECT uID FROM users WHERE uName = req.body.userName', function (error, resul) {
-        if (error) throw error;
-        res.uId = resul;
+      var user = results[0];
+      bcrypt.compare(userName + password, user.password, (err, result) => {
+        if (result === true) {
+          res.json({
+            userID: user.uId
+          });
+        } else {
+          // throw error
+        }
       });
     }
   });
-  console.log(req);
+  connection.end();
 });
 
 // req: the username and password of a new user
 // res: user object for newly added user, session token, and cookie
 // next: if error, error handler route
 router.post("/register", function(req, res, next) {
-  
   connection.connect();
-  connection.query('SELECT uName FROM users WHERE uName=req.body.userName', function (error, results) {
-    if (error) throw error;
-    if (results.length > 0) res.body.error = 'User already exists.';
-    else {
-      connection.query('INSERT INTO users(uName, password) VALUES (req.body.userName, req.body.password)', function (error, results) {
-        if (error) throw error;
-        res.uId = results;
-      });
+  connection.query(
+    `SELECT uName FROM users WHERE uName=${req.body.userName}`,
+    function(error, results) {
+      if (error) throw error;
+      if (results.length > 0) res.body.error = "User already exists.";
+      else {
+        bcrypt.hash(
+          req.body.userName + req.body.password,
+          saltRounds,
+          (err, hash) => {
+            const hashedPassword = hash;
+          }
+        );
+        connection.query(
+          `INSERT INTO users(uName, password) VALUES (${req.body.userName}, ${hashedPassword})`,
+          function(error, results) {
+            if (error) throw error;
+            res.json = {
+              user: results
+            };
+          }
+        );
+      }
     }
-  });
+  );
   connection.end();
-  
-  console.log(req);
 });
 
 // req: the userid of a user
