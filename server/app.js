8eda682946e2333346b57655ca77d65d453aa657
michaela-williams <michaela.williams359@gmail.com>
2019-11-18 16:36:51 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -29,6 +29,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/auth", authRouter);
 app.use("/users", usersRouter);
+app.use("/orders", ordersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
