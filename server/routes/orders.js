354cd7f08d2311b5a850a9e4bcf4832514b0b532
michaela-williams <michaela.williams359@gmail.com>
2019-11-18 17:06:35 -0500

    Update orders.js
    Added routes for changing order status of a given orderId.

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -11,6 +11,7 @@ const pool = mysql.createPool({
 
 var router = express.Router();
 
+//serve all orders
 router.get("/", (req, res) => {
     const allItemsString = 'SELECT * FROM orders;';
     pool.getConnection((err, connection) => {
@@ -20,6 +21,42 @@ router.get("/", (req, res) => {
             if (error) {console.log(error.message); throw error;}
             console.log(results);
             //send data to client using json
+        });
+    });
+});
+
+//change status of orderId record to "cancelled"
+router.get("/cancel:orderId", (req, res) => {
+    pool.getConnection((err, connection) => {
+        if (err) {console.log(err.message); throw err; }
+        const cancelString = 'UPDATE orders SET status = "cancelled" WHERE orderId="${req.params.orderId}";'
+        connection.query(cancelString, function(error, results) {
+            if (error) {console.log(error.message); throw error; }
+            console.log("Record changed.");
+        });
+    });
+});
+
+//change status of orderId record to "pending"
+router.get("/pending:orderId", (req, res) => {
+    pool.getConnection((err, connection) => {
+        if (err) {console.log(err.message); throw err; }
+        const pendingString = 'UPDATE orders SET status = "pending" WHERE orderId="${req.params.orderId}";'
+        connection.query(pendingString, function(error, results) {
+            if (error) {console.log(error.message); throw error; }
+            console.log("Record changed.");
+        });
+    });
+});
+
+//change status of orderId to "shipped"
+router.get("/shipped:orderId", (req, res) => {
+    pool.getConnection((err, connection) => {
+        if (err) {console.log(err.message); throw err; }
+        const shippedString = 'UPDATE orders SET status = "shipped" WHERE orderId="${req.params.orderId}";'
+        connection.query(shippedString, function(error, results) {
+            if (error) {console.log(error.message); throw error; }
+            console.log("Record changed.");
         });
     });
 });
