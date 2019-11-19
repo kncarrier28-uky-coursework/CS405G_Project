969fe334395cfe38723388b0a028fe1d09775cb6
Kenton Carrier <kncarrier28@gmail.com>
2019-11-19 17:59:42 -0500

    fix: Fat fingered the url

--- client/src/fetchAPI.js
+++ client/src/fetchAPI.js
@@ -1,6 +1,6 @@
 let apiUrl;
 if (process.env.NODE_ENV === "production")
-  apiUrl = "http://knca244.cd.uky.edu:3010";
+  apiUrl = "http://knca244.cs.uky.edu:3010";
 else apiUrl = "http://localhost:3010";
 
 export default apiUrl;
