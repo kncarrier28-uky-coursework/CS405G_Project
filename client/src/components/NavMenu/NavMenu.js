5d40466e10bee5c043ac59de61768ef390bc9224
Kenton Carrier <kncarrier28@gmail.com>
2019-11-18 23:30:50 -0500

    feat: Add links to navbar

--- client/src/components/NavMenu/NavMenu.js
+++ client/src/components/NavMenu/NavMenu.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Navbar, Heading } from "react-bulma-components";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
 export class NavMenu extends React.Component {
   render() {
     return (
@@ -9,7 +12,28 @@ export class NavMenu extends React.Component {
           <Navbar.Item renderAs="a" href="#">
             <Heading size={3}>ToyzRUs</Heading>
           </Navbar.Item>
+          <Navbar.Burger className="has-text-dark has-text-weight-semibold" />
         </Navbar.Brand>
+        <Navbar.Menu>
+          <Navbar.Container>
+            <Navbar.Item
+              href="/items"
+              className="has-text-dark has-text-weight-semibold"
+            >
+              Home
+            </Navbar.Item>
+          </Navbar.Container>
+          <div className="navbar-end">
+            <Navbar.Item
+              href="/cart"
+              className="has-text-dark has-text-weight-semibold"
+            >
+              <p>
+                Cart <FontAwesomeIcon icon={faShoppingCart} />
+              </p>
+            </Navbar.Item>
+          </div>
+        </Navbar.Menu>
       </Navbar>
     );
   }
