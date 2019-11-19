f5a6d63925d017af182bb503d8fb46f6e81e54a9
Kenton Carrier <kncarrier28@gmail.com>
2019-11-18 21:22:45 -0500

    chore: Apply prettier to items file

--- server/routes/items.js
+++ server/routes/items.js
@@ -6,51 +6,60 @@ const express = require("express");
 const mysql = require("mysql");
 
 const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: "localhost",
-    user: "geoffrey",
-    password: "Ya9pQcM3x7BsRqDX",
-    database: "toyzrus"
-  });
+  connectionLimit: 10,
+  host: "localhost",
+  user: "geoffrey",
+  password: "Ya9pQcM3x7BsRqDX",
+  database: "toyzrus"
+});
 
-  var router = express.Router();
+var router = express.Router();
 
-  router.get("/", (req, res) => {
-      const queryString = 'SELECT * FROM items;';   //all rows in items database
-      pool.getConnection((err, connection) => {
-        if (err) {console.log(err.message); throw err; }
-        connection.query(queryString, function(error, results) {
-          connection.release;
-          if (error) { console.log (error.message); throw error; }
-          else {
-            console.log(results);
-            //send data to client using json string
-            res.json(results);
-            //console.log(res.json);
-          }
-        });
-      });
-
-      //send in array (json)
-  });
-
-  router.get("/:itemId", (req, res) => {
-    pool.getConnection((err, connection) => {
-      if (err) {console.log(err.message); throw err; }
-      const queryString = `SELECT * FROM items WHERE itemId="${req.params.itemId}";`;
-      connection.query(queryString, function(error, results) {
-        connection.release;
-        if (error) {console.log(error.message); throw error; }
-        else {
-          console.log(results);
-          //send data to client using json string
-          res.json(results);
-        }
-      });
+router.get("/", (req, res) => {
+  const queryString = "SELECT * FROM items;"; //all rows in items database
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.log(err.message);
+      throw err;
+    }
+    connection.query(queryString, function(error, results) {
+      connection.release;
+      if (error) {
+        console.log(error.message);
+        throw error;
+      } else {
+        console.log(results);
+        //send data to client using json string
+        res.json(results);
+        //console.log(res.json);
+      }
     });
-
-    console.log(req.params.itemId);
   });
 
-  module.exports = router;
- 
+  //send in array (json)
+});
+
+router.get("/:itemId", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.log(err.message);
+      throw err;
+    }
+    const queryString = `SELECT * FROM items WHERE itemId="${req.params.itemId}";`;
+    connection.query(queryString, function(error, results) {
+      connection.release;
+      if (error) {
+        console.log(error.message);
+        throw error;
+      } else {
+        console.log(results);
+        //send data to client using json string
+        res.json(results);
+      }
+    });
+  });
+
+  console.log(req.params.itemId);
+});
+
+module.exports = router;
