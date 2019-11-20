9ad3776634ce24e3735439a46bab77508755b98c
michaela-williams <michaela.williams359@gmail.com>
2019-11-19 21:39:16 -0500

    Fixed app.js conflict with master

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var authRouter = require("./routes/auth");
+var ordersRouter = require("./routes/orders");
 var itemsRouter = require("./routes/items");
 var cartRouter = require("./routes/cart");
 
@@ -31,6 +32,7 @@ app.use("/", indexRouter);
 app.use("/auth", authRouter);
 app.use("/items", itemsRouter);
 app.use("/users", usersRouter);
+app.use("/orders", ordersRouter);
 app.use("/cart", cartRouter);
 
 // catch 404 and forward to error handler
