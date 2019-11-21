765bc637657a8e95a1628e0513a9d34fb3a4746d
Kenton Carrier <kncarrier28@gmail.com>
2019-11-21 12:21:43 -0500

    chore: Remove call to orders router that isn't present in this branch

--- server/app.js
+++ server/app.js
@@ -8,7 +8,6 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var authRouter = require("./routes/auth");
-var ordersRouter = require("./routes/orders");
 var itemsRouter = require("./routes/items");
 var cartRouter = require("./routes/cart");
 
@@ -32,7 +31,6 @@ app.use("/", indexRouter);
 app.use("/auth", authRouter);
 app.use("/items", itemsRouter);
 app.use("/users", usersRouter);
-app.use("/orders", ordersRouter);
 app.use("/cart", cartRouter);
 
 // catch 404 and forward to error handler
