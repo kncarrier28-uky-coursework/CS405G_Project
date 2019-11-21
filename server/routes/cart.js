3da810d6f68a6e55586209aaeab508d26117d8fb
Kenton Carrier <kncarrier28@gmail.com>
2019-11-21 12:22:10 -0500

    refactor: Apply prettier and add check cart middleware

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -3,90 +3,109 @@ const mysql = require("mysql");
 const randomstring = require("randomstring");
 
 const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: "localhost",
-    user: "geoffrey",
-    password: "Ya9pQcM3x7BsRqDX",
-    database: "toyzrus"
-  });
+  connectionLimit: 10,
+  host: "localhost",
+  user: "geoffrey",
+  password: "Ya9pQcM3x7BsRqDX",
+  database: "toyzrus"
+});
 
-  var router = express.Router();
+var router = express.Router();
 
-  router.post("/add", (req, res) => {
-      const itemId = req.body.itemId;
-      const quantity = req.body.quantity;
-      const userId = req.body.userId;
-      const findOpenCart = `SELECT orderNumber FROM orders WHERE uId=${userId} AND status="open";`; 
-
-      pool.getConnection((err, connection) => {
-          connection.query(findOpenCart, function(error, results) {
+router.use("/:userId*", (req, res, next) => {
+  const userId = req.params.userId;
+  const findOpenCart = `SELECT orderNumber FROM orders WHERE uId=${userId} AND status="open";`;
+  pool.getConnection((err, connection) => {
+    connection.query(findOpenCart, function(error, results) {
+      connection.release();
+      if (error) throw error;
+      var item_existing = 0;
+      if (results.length != 1) {
+        //no open cart exists
+        var new_orderNum = randomstring.generate(15);
+        const createCart = `INSERT INTO orders(uId, status, orderNumber) VALUES(${userId}, "open", "${new_orderNum}");`;
+        pool.getConnection((err, connection) => {
+          connection.query(createCart, function(error, results) {
             connection.release();
             if (error) throw error;
-            var orderId;
-            var item_existing = 0;
-            if (results.length != 1) { //no open cart exists
-                var new_orderNum = randomstring.generate(15);
-                const createCart = `INSERT INTO orders(uId, status, orderNumber) VALUES(${userId}, "open", "${new_orderNum}");`;
-                pool.getConnection((err, connection) => {
-                    connection.query(createCart, function(error, results) {
-                        connection.release();
-                        if (error) throw error;
-                        orderNumber = results.insertId;
-                    });
-                });
-            } //create open cart if none exists
-            else {
-                orderId = results[0].orderId;
-                const getExisting = `SELECT quantity FROM orders WHERE orderId=${orderId} AND itemId=${itemId} AND orderNumber="${orderNumber}";`;
-                pool.getConnection((err, connection) => {
-                    connection.query(getExisting, function(error, results) {
-                        connection.release();
-                        if (error) throw error;
-                        item_existing = results[0].quantity;
-                    });
-                });
-            } //get orderNumber if open cart exists
-            item_existing += quantity;
-            const add_item = `UPDATE orders SET quantity=${item_existing} WHERE itemId=${itemId} AND orderNumber="${orderNumber}";`
-            pool.getConnection((err, connection) => {
-                connection.query(add_item, function(error, results) {
-                    connection.release();
-                    if (error) throw error;
-                });
-            });
+            req.orderNumber = new_orderNum;
+            next();
           });
-      });
-  });
-
-  router.post("/remove", (req, res) => {
-      pool.getConnection((err, connection) => {
-        const itemId = req.body.itemId;
-        const quantity = req.body.quantity;
-        const userId = req.body.userId;
-        var item_existing;
-        
-        const findCartquanity = `SELECT quantity FROM orders WHERE itemId=${itemId} AND uId=${userId} AND status="open";`;
-        connection.query(findCartquantity, function(error, results) {
-            connection.release();
-            if (results.length == 0) {
-                res.json({
-                    error: "Item not in cart."
-                });
-                console.log("Error: Item not in cart.");
-                return;
-            }
-            item_existing = results[0].quantity;
         });
-        item_existing -= quantity;
-        var updateItem;
-        if (item_existing <= 0) 
-            updateItem = `DELETE FROM orders WHERE itemId=${itemId} AND uId=${userId} AND status="open";`
-        else updateItem = `UPDATE orders SET quantity=${item_existing} WHERE itemId=${itemId} AND uId=${userId} AND status="open";`
-        connection.query(updateItem, function(error, results) {
-            connection.release();
-            console.log("Items removed.");
-        });
+      } //create open cart if none exists
+      else {
+        req.orderNumber = results[0].orderNumber;
+        next();
+      }
     });
+  });
+});
+
+router.get("/:userId", (req, res) => {
+  const getUserCart = `SELECT * FROM orders WHERE orderNumber="${req.orderNumber}"`;
+  console.log(getUserCart);
+  pool.getConnection((err, connection) => {
+    connection.query(getUserCart, (error, results) => {
+      connection.release();
+      console.log(results);
+    });
+  });
+});
+
+router.post("/:userId/add", (req, res) => {
+  const itemId = req.body.itemId;
+  const quantity = req.body.quantity;
+
+  orderId = results[0].orderId;
+  const getExisting = `SELECT quantity FROM orders WHERE orderId=${orderId} AND itemId=${itemId} AND orderNumber="${orderNumber}";`;
+  pool.getConnection((err, connection) => {
+    connection.query(getExisting, function(error, results) {
+      connection.release();
+      if (error) throw error;
+      item_existing = results[0].quantity;
+    });
+  });
+  //get orderNumber if open cart exists
+  item_existing += quantity;
+  const add_item = `UPDATE orders SET quantity=${item_existing} WHERE itemId=${itemId} AND orderNumber="${orderNumber}";`;
+  pool.getConnection((err, connection) => {
+    connection.query(add_item, function(error, results) {
+      connection.release();
+      if (error) throw error;
+    });
+  });
+});
+
+router.post("/:userId/remove", (req, res) => {
+  pool.getConnection((err, connection) => {
+    const itemId = req.body.itemId;
+    const quantity = req.body.quantity;
+    const userId = req.body.userId;
+    var item_existing;
+
+    const findCartquanity = `SELECT quantity FROM orders WHERE itemId=${itemId} AND uId=${userId} AND status="open";`;
+    connection.query(findCartquantity, function(error, results) {
+      connection.release();
+      if (results.length == 0) {
+        res.json({
+          error: "Item not in cart."
+        });
+        console.log("Error: Item not in cart.");
+        return;
+      }
+      item_existing = results[0].quantity;
+    });
+    item_existing -= quantity;
+    var updateItem;
+    if (item_existing <= 0)
+      updateItem = `DELETE FROM orders WHERE itemId=${itemId} AND uId=${userId} AND status="open";`;
+    else
+      updateItem = `UPDATE orders SET quantity=${item_existing} WHERE itemId=${itemId} AND uId=${userId} AND status="open";`;
+    connection.query(updateItem, function(error, results) {
+      connection.release();
+      console.log("Items removed.");
+    });
+  });
 });
 
 module.exports = router;
