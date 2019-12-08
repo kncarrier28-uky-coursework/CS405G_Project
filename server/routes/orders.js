05b9723dbb249b1e92d1ebf10a303618382d5d46
michaela-williams <michaela.williams359@gmail.com>
2019-12-08 15:04:08 -0500

    Updating shipped route in orders, removed from staff

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -140,7 +140,6 @@ router.post("/pending", (req, res) => {
             }
           });
         }
-        connection.release();
       }
     );
   });
@@ -153,18 +152,50 @@ router.post("/shipped", (req, res) => {
       console.log(err.message);
       throw err;
     }
-    const shippedString = `UPDATE orders SET status = "shipped" WHERE orderNumber="${req.body.orderNumber}";`;
-    connection.query(shippedString, function(error, results) {
-      connection.release();
+    console.log("in ship");
+    const getQuantities = `SELECT itemId, quantity FROM orders WHERE orderNumber="${req.body.orderNumber}";`;
+    connection.query(getQuantities, function(error, results) {
+      //connection.release();
       if (error) {
         console.log(error.message);
         throw error;
-      } else {
-        console.log("Record changed.");
-        res.end();
       }
-    });
-  });
-});
+      var items_needed = [];  
+      for (i = 0; i < results.length; i++) {
+        items_needed.push([results[i].itemId, results[i].quantity]);
+      }
+      console.log(items_needed);
+      var getInventoryStock = `SELECT itemId, stock FROM items WHERE`;
+      for (i = 0; i < results.length; i++) {
+        getInventoryStock += (" itemId=" + items_needed[i][0]);
+      }
+      getInventoryStock += ';';
+      connection.query(getInventoryStock, function(error, results) {
+        //connection.release();
+        if (error) {
+          console.log(error.message);
+          throw error;
+        }
+        var items_missing = [];
+        var cur_index = 0;
+        for (i = 0; i < results.length; i++) {
+          if (results[i].stock < items_needed[i][1]) {
+            items_missing[cur_index][0] = results[i].itemId;
+            items_missing[cur_index][1] = (items_needed[i][1] - results[i].stock);
+            cur_index += 1;
+          }
+        }
+        if (items_missing.length !== 0) {
+          console.log("missing items");
+        }
+        else {
+          console.log("order shipped");
+          //remove items from inventory, change status to shipped
+        }
+        res.json(items_missing);
+      })
+    })
+  })
+})
 
 module.exports = router;
--- server/routes/staff.js
+++ server/routes/staff.js
@@ -11,56 +11,7 @@ const pool = mysql.createPool({
 
 var router = express.Router();
 
-router.post("/ship", (req, res) => {
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.log(err.message);
-      throw err;
-    }
-    const getQuantities = `SELECT itemId, quantity FROM orders WHERE orderNumber=${req.body.orderNumber};`;
-    connection.query(getQuantities, function(error, results) {
-      connection.release();
-      if (error) {
-        console.log(error.message);
-        throw error;
-      }
-      var items_needed = [];  
-      for (i = 0; i < results.length; i++) {
-        items_needed[i][0] = results[i].itemId;
-        items_needed[i][1] = results[i].quantity;
-      }
-      var getInventoryStock = `SELECT itemId, stock FROM items WHERE`;
-      for (i = 0; i < results.length; i++) {
-        getInventoryStock += (" " + items_needed[i][0]);
-      }
-      getInventoryStock += ';';
-      connection.query(getInventoryStock, function(error, results) {
-        connection.release();
-        if (error) {
-          console.log(error.message);
-          throw error;
-        }
-        var items_missing = [];
-        var cur_index = 0;
-        for (i = 0; i < results.length; i++) {
-          if (results[i].stock < items_needed[i][1]) {
-            items_missing[cur_index][0] = results[i].itemId;
-            items_missing[cur_index][1] = (items_needed[i][1] - results[i].stock);
-            cur_index += 1;
-          }
-        }
-        if (items_missing.length !== 0) {
-          console.log("missing items");
-        }
-        else {
-          console.log("order shipped");
-          //remove items from inventory, change status to shipped
-        }
-        res.json(items_missing);
-      })
-    })
-  })
-})
+
 
 
 module.exports = router;
