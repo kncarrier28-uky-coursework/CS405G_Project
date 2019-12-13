dc15b38b4d259783f12f7f76f07304c0f9134335
michaela-williams <michaela.williams359@gmail.com>
2019-12-13 02:26:37 -0500

    It works?

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -3,10 +3,14 @@ import apiUrl from "../fetchAPI";
 import { ItemList } from "../components/Items";
 
 export class SearchPage extends React.Component {
+   
+   
     constructor(props) {
         super(props);
         this.state = {
-            items: []
+            items: [],
+            searched: false,
+            searchQuery: ""
         };
         this.fetchSearchedItems = this.fetchSearchedItems.bind(this);
         this.handleChange = this.handleChange.bind(this);
@@ -23,51 +27,65 @@ export class SearchPage extends React.Component {
         this.fetchSearchedItems();
         this.fetchSearchedItems();
     }
-    fetchSearchedItems(searchQuery) {
-        fetch(apiUrl + `/search/${searchQuery}`, {
+    fetchSearchedItems() {
+       // if (this.state.searched) {
+            this.setState.searched = false;
+        fetch(apiUrl + `/search/${this.state.searchQuery}`, {
             method: "GET",
             credentials: "same-origin",
             headers: {
                 "Content-type": "application/json"
             },
         })
+    
             .then(res => res.json())
-            .then(data => {this.setState({ items: data })
+            .then(data => {if (data.length) this.setState({ items: data })
             .then()
     })
             .catch(error => console.log(error));
-            
+        //}
+                    
     }
-   
+    
     render() {
-        return (<>
-            <div className="level">
-                <div className="level-item">
-                    <p className="title">Search</p>
-                </div>
-            </div>
-            <div className="field is-grouped">
-                <div className="field">
-                    <label className="label">Search</label>
-                    <div className="control">
-                        <input className="input"
-                            type="text" name="searchQuery" 
-                            placeholder="Search by Category or Keyword" 
-                            onChange={this.handleChange} 
-                        />
-                    </div>
-                    <div className="control">
-                        <label className="label">&nbsp;</label>
-                        <button className="button is-primary" onClick={this.fetchSearchedItems}>
-                            Search
-              </button>
+        
+            return(<>
+        
+                <div className="level">
+                    <div className="level-item">
+                        <p className="title">Search</p>
                     </div>
                 </div>
-            </div>
+                <div className="field is-grouped">
+                    <div className="field">
+                        <label className="label">Search</label>
+                        <div className="control">
+                            <input className="input"
+                                type="text" name="searchQuery" 
+                                placeholder="Search by Category or Keyword" 
+                                onChange={this.handleChange} 
+                            />
+                        </div>
+                        <div className="control">
+                            <label className="label">&nbsp;</label>
+                            <button className="button is-primary" onClick={this.fetchSearchedItems()}>
+                                Search
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <ItemList
+          userId={this.props.userId}
+          items={this.state.items}
+          refreshItems={this.fetchSearchedItems}
+          inCart={false}
+        />
+                
+            </>
 
-        </>
-        );
+            );
+        }
     }
-}
+
 
 export default SearchPage;
